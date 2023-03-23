21ee88e981933d427d0519e0e8410ba151774c61
Quantum Snowball <quantum.snowball@gmail.com>
2023-03-24 01:34:31 +0800

    proj: setup.py as mlbox package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mlbox',
+    version='0.1.0',
+    description='mlbox - a machine learning toolbox intended to be use along with trbox when backtesting and trading',
+    url='https://github.com/quantumsnowball/mlbox',
+    author='Quantum Snowball',
+    author_email='quantum.snowball@gmail.com',
+    license='MIT',
+    packages=['mlbox'],
+    zip_safe=False,
+)
