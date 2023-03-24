9aa50dd9a6c990cc70c4d6163c019c42fbe37d73
Quantum Snowball <quantum.snowball@gmail.com>
2023-03-24 12:50:44 +0800

    step: Agent ABC

---
+++ mlbox/agent/__init__.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
+
+T_State = TypeVar('T_State')
+T_Action = TypeVar('T_Action')
+
+
+class Agent(ABC, Generic[T_State, T_Action]):
+    def __init__(self) -> None:
+        pass
+
+    #
+    # acting
+    #
+
+    @abstractmethod
+    def decide(self,
+               state: T_State,
+               *,
+               epilson: float = 0.5) -> T_Action:
+        pass
+
+    #
+    # training
+    #
+
+    @abstractmethod
+    def update_target(self) -> None:
+        pass
+
+    @abstractmethod
+    def learn(self,
+              epochs: int = 1000,
+              batch_size: int = 512,
+              gamma: float = 0.99):
+        pass
+
+    @abstractmethod
+    def explore(self):
+        pass
+
+    @abstractmethod
+    def train(self,
+              n_eps: int = 1000):
+        pass
