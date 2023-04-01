649d77bec2d268b05c43bcd71a1c46e320e60c57
Quantum Snowball <quantum.snowball@gmail.com>
2023-04-01 13:49:36 +0800

    step: train loop basic structure of pg agent

--- mlbox/agent/pg/__init__.py
+++ mlbox/agent/pg/__init__.py
@@ -1,3 +1,4 @@
+from collections import deque
 from pathlib import Path
 
 import numpy as np
@@ -7,6 +8,7 @@ from torch.nn import Module
 from typing_extensions import override
 
 from mlbox.agent.basic import BasicAgent
+from mlbox.trenv.queue import TerminatedError
 from mlbox.types import T_Action, T_Obs
 
 
@@ -43,9 +45,58 @@ class PGAgent(BasicAgent[T_Obs, T_Action]):
     def learn(self) -> None:
         raise NotImplementedError()
 
+    n_eps = 100
+    batch_size = 6000
+    max_step = 1000
+    print_hash_every = 1
+    report_progress_every = 10
+    rolling_reward_ma = 5
+
     @override
     def train(self) -> None:
-        raise NotImplementedError()
+        self.policy_net.train()
+        # episode loop
+        rolling_reward = deque[float](maxlen=self.rolling_reward_ma)
+        for i_eps in range(1, self.n_eps):
+            # batch loop
+            while True:
+                # reset to a new environment
+                obs, *_ = self.env.reset()
+                # trajectory loop
+                for _ in range(self.max_step):
+                    # act
+                    action = self.exploit(obs)
+                    # step
+                    try:
+                        next_obs, reward, terminated, truncated, *_ = \
+                            self.env.step(action)
+                    except TerminatedError:
+                        break
+                    done = terminated or truncated
+                    # cache experience
+                    self.buffer.cache(obs, action, reward,
+                                      next_obs, terminated)
+                    # pointing next
+                    obs = next_obs
+                    if done:
+                        break
+                # post processing to cached experience before flush
+                self.buffer.assert_terminated_flag()
+                # flush cache trajectory to memory
+                self.buffer.flush()
+                # report progress
+                if i_eps % self.print_hash_every == 0:
+                    print('#', end='', flush=True)
+                # enough traj
+                if len(self.buffer) >= self.batch_size:
+                    break
+            # learn from trajectory experience replay
+            self.learn()
+            # evulate and report progress
+            if i_eps % self.report_progress_every == 0:
+                rolling_reward.append(self.play(self.max_step))
+                mean_reward = sum(rolling_reward)/len(rolling_reward)
+                print(f' | Episode {i_eps:>4d} | {mean_reward=:.1f}')
 
     #
     # acting
