921a8c302a7b2dc21db8174b04f40726ade4f615
Quantum Snowball <quantum.snowball@gmail.com>
2023-04-13 23:28:09 +0800

    solve: pg lunarlander

---
+++ playground/classic_controls/discrete/lunarlander/pg/demo.py
@@ -0,0 +1,46 @@
+import gymnasium as gym
+import numpy as np
+import numpy.typing as npt
+from gymnasium.spaces import Box, Discrete
+from torch.nn import Identity, ReLU, Tanh
+from torch.optim import Adam
+
+from mlbox.agent.pg import PGAgent
+from mlbox.agent.pg.nn import BaselineNet, PolicyNet
+from mlbox.neural import FullyConnected
+
+ENV = 'LunarLander-v2'
+
+Obs = npt.NDArray[np.float32]
+Action = np.int64
+
+
+class MyAgent(PGAgent[Obs, Action]):
+    device = 'cpu'
+    max_step = 1000
+    n_eps = 1000
+    batch_size = 3000
+    print_hash_every = 10
+    report_progress_every = 5
+    render_every = 100
+    # variant
+    reward_to_go = True
+    baseline = True
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.env = gym.make(ENV)
+        self.render_env = gym.make(ENV, render_mode='human')
+        assert isinstance(self.env.observation_space, Box)
+        assert isinstance(self.env.action_space, Discrete)
+        in_dim = self.env.observation_space.shape[0]
+        out_dim = self.env.action_space.n.item()
+        self.policy_net = PolicyNet(in_dim, out_dim).to(self.device)
+        self.baseline_net = BaselineNet(in_dim, 1).to(self.device)
+        self.policy_optimizer = Adam(self.policy_net.parameters(), lr=1e-3)
+        self.baseline_optimizer = Adam(self.baseline_net.parameters(), lr=1e-3)
+
+
+agent = MyAgent()
+agent.prompt('model.pth', )
+agent.play(agent.max_step, env=agent.render_env)
