ac1aa493cc3ae72e95532cac24d987bfc3c39811
Quantum Snowball <quantum.snowball@gmail.com>
2023-04-23 22:41:14 +0800

    step: gymenv

--- mlbox/interface/trenv/__init__.py
+++ mlbox/interface/trenv/__init__.py
@@ -1,15 +1,15 @@
-from gymnasium import Env
 from pandas import Timestamp
 from trbox.common.types import Symbol
 from trbox.market.yahoo.historical.windows import YahooHistoricalWindows
 
+from mlbox.interface.trenv.gymenv import GymEnv
 from mlbox.interface.trenv.routine import Routine
 from mlbox.types import T_Action, T_Obs
 
 
 class TrEnv(
     Routine[T_Obs, T_Action],
-    Env[T_Obs, T_Action],
+    GymEnv[T_Obs, T_Action],
 ):
     '''
     The interface for TrEnv
